c9060588cb0656dd64bd88cb78e00ec6303ba30c
hg6p <ivernjekurac@gmail.com>
2021-07-21 18:12:25 +0200

    classes

--- library.js
+++ library.js
@@ -1,106 +1,104 @@
 let library = [
   /*  new Book("The Hobbit", "by J.R.R. Tolkien", " 295 pages", "not read yet"), */
 ];
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read ? "read" : "have not read yet";
-}
-
-function getForm() {
-  return document.getElementsByTagName("form")[0];
-}
-
-function qq() {
-  getForm().style.display = "none";
-}
-getInputs = () => {
-  return document.getElementsByTagName("input");
-};
-addBookToLibrary = () => {
-  getForm().style.display = "block";
-};
-window.onclick = (event) => {
-  if (event.target == getForm()) getForm().style.display = "none";
-};
-displayBooks = () => {
-  var table = document.createElement("table");
-  var tblBody = document.createElement("tbody");
-  let r = document.createElement("tr");
-  let keys = Object.keys(library[0]);
-  for (var j = 0; j < keys.length; j++) {
-    var cell = document.createElement("td");
-    var cellText = document.createTextNode(`${keys[j]}`);
-    cell.appendChild(cellText);
-    r.appendChild(cell);
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
-  tblBody.appendChild(r);
+  get getTitle() {
+    return this.title;
+  }
+  set setTitle(name) {
+    this.title = name;
+  }
+  get getRead() {
+    return this.read ? 'read' : 'not read yet';
+  }
+  set setRead(arg) {
+    console.log(arg);
+    this.read = !arg;
+  }
+}
 
-  for (let objectValue in library) {
-    let row = document.createElement("tr");
-    let button = document.createElement("button");
-    let buttonChange = document.createElement("button");
-    for (let value of Object.values(library[objectValue])) {
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode(`${value}`);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
+const tbate = new Book('tbate', 'turtle93', '???', false);
+console.log(tbate);
+
+const domStuff = (() => {
+  const getInput = () => {
+    const inputs = document.querySelectorAll('input');
+    const input1 = inputs[0].value;
+    const input2 = inputs[1].value;
+    const input3 = inputs[2].value;
+    const input4 = inputs[3].checked;
+    return { input1, input2, input3, input4 };
+  };
+  const loadBooks = () => {
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#input-form');
+    for (let value in library) {
+      const card = document.createElement('div');
+      card.classList.add('card', 'crd-flx');
+      for (let prop in library[value]) {
+        const title = document.createElement('p');
+        title.textContent = library[value][prop];
+        card.appendChild(title);
+      }
+      const btn = document.createElement('button');
+      btn.textContent = 'Change Status';
+      card.id = value;
+      btn.addEventListener('click', (event) => {
+        event.target.setRead = event.target.setRead;
+      });
+      card.appendChild(btn);
+      container.insertBefore(card, form);
     }
-    button.setAttribute("id", objectValue);
-    button.onclick = function () {
-      removeBook(event);
-    };
-    button.appendChild(document.createTextNode("remove"));
-    row.appendChild(button);
-    buttonChange.setAttribute("id", objectValue);
-    buttonChange.onclick = function () {
-      library[objectValue].changeRead();
-      let table = document.getElementsByTagName("table");
-      table[table.length - 1].style.display = "none";
-      displayBooks();
-    };
-    buttonChange.appendChild(document.createTextNode("change read status"));
-    row.appendChild(buttonChange);
-    tblBody.appendChild(row);
-  }
-  table.appendChild(tblBody);
-  document.getElementById("shelf").appendChild(table);
-  table.setAttribute("border", "2");
-  localStorage.setItem("book", JSON.stringify(library));
-};
-addBook = () => {
-  library.push(
-    new Book(
-      getInputs()[0].value,
-      getInputs()[1].value,
-      getInputs()[2].value,
-      getInputs()[3].checked
-    )
-  );
-  qq();
-  let table = document.getElementsByTagName("table");
-  table[table.length - 1].style.display = "none";
-  displayBooks();
-  /* getForm().reset(); */
-};
-removeBook = (event) => {
-  let table = document.getElementsByTagName("table");
-  table[table.length - 1].style.display = "none";
-  library.splice(event.explicitOriginalTarget.id, 1);
-  displayBooks();
-};
-
-Book.prototype.changeRead = function () {
-  this.read = this.read == "read" ? "not read yet" : "read";
-};
-
-let data = JSON.parse(localStorage.getItem("book"));
-for (let a in data) {
-  library.push(
-    new Book(data[a].title, data[a].author, data[a].pages, data[a].read)
-  );
-}
-saveToLocal = () => {
-  localStorage.setItem("book", JSON.stringify(library));
-};
+  };
+  const pushClass = () => {
+    const input = getInput();
+    library.push(
+      new Book(input.input1, input.input2, input.input3, input.input4)
+    );
+  };
+  const addBook = () => {
+    pushClass();
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#input-form');
+    const card = document.createElement('div');
+    card.classList.add('card', 'crd-flx');
+    card.id = `card-${library.length - 1}`;
+    let inputs = library[library.length - 1];
+    for (let value in inputs) {
+      const title = document.createElement('p');
+      title.textContent =
+        inputs[value] !== inputs.read
+          ? inputs[value]
+          : inputs[value]
+          ? 'read'
+          : 'not read yet';
+      card.appendChild(title);
+    }
+    const btn = document.createElement('button');
+    btn.textContent = 'Change Status';
+    btn.addEventListener('click', (event) => {
+      console.log(event.target);
+      library[event.target.id].setRead = library[event.target.id].read;
+      let card = document.querySelectorAll(`#card-${event.target.id} p`)[3];
+      card.textContent = library[event.target.id].getRead;
+    });
+    btn.id = library.length - 1;
+    card.appendChild(btn);
+    container.insertBefore(card, form);
+  };
+  return {
+    loadBooks,
+    addBook,
+  };
+})();
+const btn = document.querySelector('form button');
+btn.addEventListener('click', () => {
+  domStuff.addBook();
+});
+domStuff.loadBooks();
